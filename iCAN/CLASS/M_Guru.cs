2a28c4ca9013ee2b4066628c26afd78014e9a12a
daniagung <daniagung.ict@gmail.com>
2017-04-29 01:40:40 +0700

    Updated Class Guru

--- iCAN/CLASS/M_Guru.cs
+++ iCAN/CLASS/M_Guru.cs
@@ -8,29 +8,46 @@ namespace iCAN.CLASS
 {
     class M_Guru : M_User
     {
-
+        private String idGuru;
         private String NIP;
-        
+        private String alamat;
+        private String no_hp;
+
+        public string myNIP { get => NIP; set => NIP = value; }
+        public string Alamat { get => alamat; set => alamat = value; }
+        public string No_hp { get => no_hp; set => no_hp = value; }
+        public string IdGuru { get => idGuru; set => idGuru = value; }
 
         public M_Guru(int IdUser) : base(IdUser)
         {
             
             Database db = new Database();
-            db.reader = db.callQuery("SELECT NIP FROM guru where id_user = " + IdUser);
+            db.reader = db.callQuery("SELECT * from v_guru where id_user =" + IdUser);
             if (db.reader.Read())
             {
-                NIP = db.reader.GetString(0);
+                myNIP = db.reader.GetString(2);
+                Alamat = db.reader.GetString(4);
+                No_hp = db.reader.GetString(5);
             }
+        }
+        public M_Guru(int idUser, String idGuru ,string nip, string nama, string alamat, string no_hp)
+        {
+            IdUser = idUser;
+            IdGuru = idGuru;
+            myNIP = nip;
+            Nama = nama;
+            Alamat = alamat;
+            No_hp = no_hp;
         }
 
         public void SetNIP(String NIP)
         {
-            this.NIP = NIP;
+            this.myNIP = NIP;
         }
 
         public String GetNIP()
         {
-            return this.NIP;
+            return this.myNIP;
         }
 
     }
